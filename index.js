ea99c4181272a46f44a5f0e7837ad911018b2f4d
xaviguasch <xaviguasch@gmail.com>
2020-08-06 13:17:29 +0200

    added starter files

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Promises - Challenge
+ * Create a promise that returns some user data and throws an error when not found.
+ * Log the user data when listening to the promise as well as log the error.
+ *
+ * Docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+ */
